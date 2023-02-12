ef644ea13949ecfa0c5fc0d132541e2ef6cb44e6
Charleston A <charleston.anjos@gmail.com>
2023-02-12 13:41:08 -0300

    create index for ViewState and ErrorBuilder

---
+++ src/pages/entrance/login/view_state/index.ts
@@ -0,0 +1,10 @@
+import {Form, Loading, ViewState, Success} from "./ViewState";
+import {ErrorBuilder} from "./ErrorBuilder";
+
+export {
+    Form,
+    Loading,
+    ViewState,
+    Success,
+    ErrorBuilder
+}
